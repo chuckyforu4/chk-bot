23e896d4cc6bbdddd64a5918bb3b80d79579324f
chuckyforu4 <chuckyforu4@gmail.com>
2025-12-15 16:49:41 +0630

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const express = require("express");
 
 const app = express();
 
-// Telegram Bot
+
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
 
 bot.on("message", (msg) => {
   bot.sendMessage(msg.chat.id, "Bot is alive ✅");
 });
 
-// Health check for Render
+
 app.get("/", (req, res) => {
   res.send("Bot running");
 });
 
-// Server
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server running on port", port);
-});
+  console.log("Server running on port", por
+
